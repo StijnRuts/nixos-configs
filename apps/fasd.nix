42cf7e3851cd4a3376e2d635560757ca4c084f58
Stijn Ruts <github@stijnruts.be>
2018-01-03 21:18:35 +0100

    Fix fasd

--- apps/fasd.nix
+++ apps/fasd.nix
@@ -2,14 +2,16 @@
 {
   environment.systemPackages = [ pkgs.fasd ];
 
-  programs.bash.shellAliases = {
-    a  = "fasd -a";       # any
-    s  = "fasd -si";      # show / search / select
-    d  = "fasd -d";       # directory
-    f  = "fasd -f";       # file
-    sd = "fasd -sid";     # interactive directory selection
-    sf = "fasd -sif";     # interactive file selection
-    z  = "fasd_cd -d";    # cd, same functionality as j in autojump
-    zz = "fasd_cd -d -i"; # cd with interactive selection
-  };
+  programs.bash.interactiveShellInit = ''
+    eval "$(${pkgs.fasd}/bin/fasd --init auto)"
+  '';
+
+  # a  = "fasd -a";       # any
+  # s  = "fasd -si";      # show / search / select
+  # d  = "fasd -d";       # directory
+  # f  = "fasd -f";       # file
+  # sd = "fasd -sid";     # interactive directory selection
+  # sf = "fasd -sif";     # interactive file selection
+  # z  = "fasd_cd -d";    # cd, same functionality as j in autojump
+  # zz = "fasd_cd -d -i"; # cd with interactive selection
 }
