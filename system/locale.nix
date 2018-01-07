e032f2cac043e2f9a958891caf0886b1de84b35d
Stijn Ruts <github@stijnruts.be>
2018-01-07 22:03:42 +0100

    More detailed locale settings

--- system/locale.nix
+++ system/locale.nix
@@ -6,8 +6,16 @@
 
   i18n.consoleFont = "Lat2-Terminus16";
 
-  # i18n.supportedLocales = ["en_US.UTF-8" "nl_BE.UTF-8"];
   i18n.defaultLocale = "en_US.UTF-8";
+
+  environment.sessionVariables = {
+    LANGUAGE = "en_US.UTF-8";
+    LC_MONETARY = "nl_BE.UTF-8";
+    LC_MEASUREMENT = "nl_BE.UTF-8";
+    LC_NUMERIC = "nl_BE.UTF-8";
+    LC_PAPER = "nl_BE.UTF-8";
+    LC_TIME = "nl_BE.UTF-8";
+  };
 
   time.timeZone = "Europe/Brussels";
 }
